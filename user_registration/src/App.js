cfa954ebe066bc8eaa5bde1b3687a3b4a24e5231
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-08-21 13:10:29 +0800

    Setting up route

--- user_registration/src/App.js
+++ user_registration/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import { UserList } from './UserList';
+import { EditUser } from './EditUser';
+import { CreateUser } from './CreateUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Users</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Add User</Link>
+            </li>
+          </ul>
+        </nav>
+      <Switch>
+        <Route exact path="/" component={UserList}/>
+        <Route path="/edit/:id" component={EditUser}/>
+        <Route path="/create" component={CreateUser}/>
+    </Switch>
     </div>
+    
   );
 }
 
---
+++ user_registration/src/CreateUser.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CreateUser = () =>{
+    return <div className="container">  Create User </div>
+}
